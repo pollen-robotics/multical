a2081daeb89ecec5618254f2de09bfb8bed9b898
apirrone <awanegenebayo@gmail.com>
2024-10-02 12:20:07 +0200

    adding boards

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup, find_namespace_packages
-
 from os import path
+
+from setuptools import find_namespace_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -36,7 +36,7 @@ setup(
         "numba",
         "scipy",
         "matplotlib",
-        "opencv-contrib-python>=4.5.0.0",
+        "opencv-contrib-python==4.8.0.74",
         "simple-parsing",
         "natsort",
         "cached-property",
